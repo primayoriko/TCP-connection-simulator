7df452c6e7a21d15cccc7e83501907a2a3db33ac
mufraswid <fauzanrafisidiq@gmail.com>
2020-09-26 13:09:13 +0700

    Add packet class and checksum func

--- Packet.py
+++ Packet.py
@@ -1,13 +1,67 @@
 # Packet data structure
 
 class Packet:
-    def __init__(self, data_type, seq_num, data):
-        self.data_type = data_type
-        self.seq_num = seq_num
+     def __init__(self, pack_type, length, seqnum, checksum, data=''):
+        self.seqnum = seqnum
+        self.length = length
         self.data = data
+        
+        if pack_type = 'DATA':
+            this.type = 0x0
+        elif pack_type = 'ACK':
+            this.type = 0x1
+        elif pack_type = 'FIN':
+            this.type = 0x2
+        elif pack_type = 'FIN-ACK':
+            this.type = 0x3
 
-        total = str(data_type) + str(seq_num) + str(data)
-        self.checksum = 0
+        self.checksum = Packet.checksum(self)
 
-        for i in range(total.length, ):
-            pass
+
+    # Comparison operators
+    def __lt__(self, other):
+        return self.seqnum < other.seqnum
+    
+    def __gt__(self, other):
+        return self.seqnum > other.seqnum
+
+    # Flag 
+    def is_fin(self):
+        return this.type == 0x2
+    
+    def is_finack(self):
+        return this.type == 0x3
+    
+    def is_ack(self):
+        return this.type == 0x1
+
+    def is_data(self):
+        return this.type == 0x0
+        
+    # Static Packet util functions
+    @staticmethod
+    def identify_type(num):
+        if num == 0x0:
+            return 'DATA'
+        elif num == 0x1:
+            return 'ACK'
+        elif num == 0x2:
+            return 'FIN'
+        elif num == 0x3:
+            return 'FIN-ACK'
+
+    @staticmethod
+    def checksum(packet):
+        checksum = 0
+
+        # Checksum attributes
+        checksum = checksum ^ packet.type ^ packet.length ^ packet.seqnum
+        
+        # Checksum all 16-bit block of data
+        data_bits = int.from_bytes(packet.data, byteorder='big')
+
+        while data_bits:
+            checksum ^= data_bits & 0xFFFF
+            data_bits = data_bits >> 16
+        
+        return checksum
