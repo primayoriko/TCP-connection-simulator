0e899f843822df2ca2e3b6cdaf4baea485ff4bf5
Naufal Prima Yoriko <primayoriko@gmail.com>
2020-09-28 14:17:37 +0700

    changing sender

--- run_sender.sh
+++ run_sender.sh
@@ -5,5 +5,6 @@
 # $2 berisi port receiver (akan diisi oleh autograder)
 # $3 berisi path file yang akan dikirim (akan diisi oleh autograder)
 # Contoh: echo -e "$1\n$2\n$3" | python3 sender.py
+# echo "Sender"
 
-echo "Sender"
+echo -e "$1\n$2\n$3" | python3 sender.py
--- sender.py
+++ sender.py
@@ -1,48 +1,68 @@
-# from Packet import Packet
 import socket
 import sys
-from packet import Packet
+import time
 from filemanager import FileManager
+# from packet import Packet
 
 HOST = '127.0.0.1'
 PORT = 1338
 MAX_SEG_SIZE = 32774
-TIMEOUT = 1000
+TIMEOUT = 3
 
-if __name__ == '__main__':
+def get_time_millis():
+    return int(round(time.time() * 1000))
+
+def run():
+    # Get parameter from command-line (*NEED TO BE FIXED*)
     file_path = sys.argv[1]
     targets = sys.argv[2:]
 
-    file_manager = FileManager()
-    file_manager.addFile(file_path)
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(TIMEOUT)
     sock.bind((HOST, PORT))
-    sock.listen()
     
     print(file_path)
     print(targets)
-    print(f'File input checksum: 0x{file_manager.checksum:04x}')
-    print(file_manager.numpackets)
 
     # Sent packets one by one per target 
     for target in targets:
+        file_manager = FileManager()
+        file_manager.addFile(file_path)
         success = 0
         arr_succeed = [False for i in range(file_manager.numpackets)]
 
+        print(f'File input checksum: 0x{file_manager.checksum:04x}')
+        print(file_manager.numpackets)
+
         # Loop until all data sent
-        while(not all(arr_succeed)):
+        while(success < file_manager.numpackets):
             for i in range(file_manager.numpackets):
                 if(not arr_succeed[i]):
-                    s.connect((HOST, PORT))
                     sent = False
+                    # s.connect((HOST, PORT))
+
+                    # Looping packet until successfully sent
                     while(not sent):
-                        s.sendall(target)
+                        s.sendto(Packet.to_bytes(file_manager.data[i]),
+                                    (HOST, PORT)
+                                )
+                        try:
+                            conn, addr = socket.recvfrom(MAX_SEG_SIZE)
+                            packet = Packet.from_bytes(data)
+                            if(packet.is_ack()):
+                                sent = True
+                                break
+                        except socket.Timeout:
+                            pass
+
                     arr_succeed[i] = sent
                     success += 1 
+
             print("Status : " + str(success) + "/" + 
                     str(file_manager.numpackets) + " packet(s) sent"
-                    )
-            pass
-
+                )
+            print("Details : " +str(arr_succeed))
         pass
+
+if __name__ == '__main__':
+    run()
